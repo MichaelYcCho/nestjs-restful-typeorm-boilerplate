60df67c1a1ee57bff4d017fb553b8fb47fe0fbd4
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-01-16 22:06:19 +0900

    feat: TimeStamp Entity

---
+++ src/core/entities/time-stamp.entity.ts
@@ -0,0 +1,9 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export abstract class TimeStamp {
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt: Date;
+}
---
+++ src/core/utils/constant.ts
@@ -0,0 +1,3 @@
+export const USER_PREFIX = 'users'
+export const JWT_PREFIX = 'jwt'
+export const DB_SCHEMA = 'public'

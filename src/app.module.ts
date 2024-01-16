932a2c304fb7011f084edad83fc99810cbaee917
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-01-16 21:44:02 +0900

    chore: Typeorm Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from '@config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./env/.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        typeORMConfig(configService),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
